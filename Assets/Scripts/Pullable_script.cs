301da1a82fdf009133066dcf0d9f40ebd68950f2
Noothowl <martinchipoco@gmail.com>
2022-12-19 17:31:30 -0300

    Release and grab final v1

--- Assets/Scripts/Pullable_script.cs
+++ Assets/Scripts/Pullable_script.cs
@@ -5,21 +5,25 @@ using UnityEngine;
 public class Pullable_script : MonoBehaviour
 {
     protected Rigidbody rb;
+    protected GameObject pb;
+
     // Start is called before the first frame update
     void Start()
     {
-        rb = gameObject.GetComponentInChildren<Rigidbody>();
+        pb = GameObject.Find("Physic_obj");
+        rb = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+
     }
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.CompareTag("Hook"))
+        if (other.CompareTag("Hook")&& Input.GetKey(KeyCode.Space))
         {
             // attach to the hook
           
@@ -28,6 +32,14 @@ public class Pullable_script : MonoBehaviour
 
             transform.position = other.transform.GetChild(1).position;
             //transform.parent = other.transform;
+        }
+        else
+        {   
+            
+
+            
+            rb.useGravity = true;
+            rb.isKinematic = false;
         }
     }
 }

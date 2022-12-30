fbea20c80beaee42326209784d1919ecb135a705
Noothowl <martinchipoco@gmail.com>
2022-12-30 10:43:28 -0300

    Added Enum Object cat. | UI, non finished logical manager

--- Assets/Scripts/Pullable_script.cs
+++ Assets/Scripts/Pullable_script.cs
@@ -2,10 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class Pullable_script : MonoBehaviour
 {
     protected Rigidbody rb;
     protected GameObject pb;
+    protected int Obj_score;
+
+    [SerializeField] private Obj_Type obj_Type;
+
+    enum Obj_Type
+    {
+        Madera,Concreto,Otros
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -17,13 +27,13 @@ public class Pullable_script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ScorePoints();
 
     }
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.CompareTag("Hook")&& Input.GetKey(KeyCode.Space))
+        if (other.CompareTag("Hook") && Input.GetKey(KeyCode.Space))
         {
             // attach to the hook
           
@@ -32,6 +42,7 @@ public class Pullable_script : MonoBehaviour
 
             transform.position = other.transform.GetChild(1).position;
             //transform.parent = other.transform;
+            Debug.Log("Ptje: "+Obj_score+" tipo: "+obj_Type);
         }
         else
         {   
@@ -40,6 +51,27 @@ public class Pullable_script : MonoBehaviour
             
             rb.useGravity = true;
             rb.isKinematic = false;
+        }
+    }
+
+
+
+    private void ScorePoints() 
+    {
+        switch (obj_Type)
+        {
+            case Obj_Type.Madera:
+                Obj_score = 15;
+                break;
+            case Obj_Type.Concreto:
+                Obj_score = 35;
+                break;
+            case Obj_Type.Otros:
+                Obj_score = 5;
+                break;
+
+            default:
+                break;
         }
     }
 }
